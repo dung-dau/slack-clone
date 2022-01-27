2774d31747f7c200104709628e128e57966ddd77
Dung Dau <dudau0924222@gmail.com>
2022-01-26 20:59:25 -0500

    Avatar, time and search icons added to header bar

---
+++ slack-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCOaTsATgqdkyqAqNfby1AooqD90TVxIN0",
+    authDomain: "slack-clone-5e2e0.firebaseapp.com",
+    projectId: "slack-clone-5e2e0",
+    storageBucket: "slack-clone-5e2e0.appspot.com",
+    messagingSenderId: "570236163081",
+    appId: "1:570236163081:web:cc2c964de09160acbd14d8"
+};
